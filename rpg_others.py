1e5af0b2d442a16e99db54a33f045dc9fd3c3a03
Juan J <shurkun@gmail.com>
2022-05-15 23:00:00 +0200

    fixed speed for sending the monster

--- rpg_others.py
+++ rpg_others.py
@@ -1,48 +1,82 @@
-import time
-from random import choice, randint
+""" generates random monster for Microlite20. Adapted from 1d4chan"""
+from random import randint, choice
+from math import floor
+from re import compile, search
 from telegram import Update, ChatAction
 from telegram.ext import CallbackContext
-from datos import rpg_list
+from time import sleep
 
 
-def roll(update: Update, context: CallbackContext):
-    """Lanza dados usando el formato XdY."""
+np = {
+    'W': 'CT CT CX CDF CVFT CDFU CTU IT ICT A',
+    'A': 'KVKVtion',
+    'K': 'b c d f g j l m n p qu r s t v sP',
+    'I': 'ex in un re de',
+    'T': 'VF VEe',
+    'U': 'er ish ly en ing ness ment able ive',
+    'C': 'b c ch d f g h j k l m n p qu r s sh t th v w y sP Rr Ll',
+    'E': 'b c ch d f g dg l m n p r s t th v z',
+    'F': 'b tch d ff g gh ck ll m n n ng p r ss sh t tt th x y zz rR sP lL',
+    'P': 'p t k c',
+    'Q': 'b d g',
+    'L': 'b f k p s',
+    'R': 'P Q f th sh',
+    'V': 'a e i o u',
+    'D': 'aw ei ow ou ie ea ai oy',
+    'X': 'e i o aw ow oy'
+}
+
+atktypes = 'Bite Claw Slam Gore Sting Tentacle Shock Broadsword Battleaxe Club Glaive Spear Falchion Dagger'
+
+
+
+
+def new_word():
+    # Generates a random name with some clever regex golf
+    word = 'W'
+    p = compile('[A-Z]')
+    while len(word) < 40:
+        match = search(p, word)
+        if match:
+            matched = match.group(0)
+        else:
+            return word.capitalize()
+        word = word.replace(matched, choice(np[matched].split(' ')))
+    return word.capitalize()
+
+
+def micro_monster(hd):
+    # Generates stats and formats output
+    construct = f"Name: {new_word()}\n"
+    construct += f"HD: {hd} \({int(floor(hd * 4.5)) + randint(0, hd * 4)}hp\), "
+    construct += f"AC: {randint(0, 5) + hd + 10}, "
+    construct += f"Attack: {choice(atktypes.split(' '))} \+ { hd + randint(0, 4)} "
+    construct += f"\({randint(0, 2) + 1}d{2 * (randint(0, 6) + 1)}\)'\n"
+    # print(construct)
+    return construct
+
+
+'''hd = int(input("Enter number of hit dice(whole number): "))
+micro_monster(hd)'''
+
+
+def bestiary_microlite(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
     try:
-        die = update.message.text.replace('/roll', '').replace('@YOURBOT', '').lower().split('d')  # TODO @YOURBOT
-        quantity, faces = int(die[0]), int(die[1])
-        context.bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING, timeout=20)
-        time.sleep(3)
-        result = []
-        if (1 < faces < 101) and (0 < quantity < 101):
-            for i in range(quantity):
-                rolls = randint(1, faces)
-                result.append(rolls)
-            suma = sum(result)
-            update.message.reply_text(text=f'Tu tirada ha sido de:\n_*{result}*_\nCon un valor total de: _*{suma}*_',
+        hd = int(update.message.text.replace('/micromonster', '').replace('@ShurkunMK2Bot', ''))
+        if hd > 0:
+            monster = micro_monster(hd=hd)
+            context.bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING, timeout=15)
+            sleep(10)
+            update.message.reply_text(text=f'Tu monstruo con HD *{hd}* es:\n\n_{monster}_',
                                       parse_mode='MarkdownV2')
         else:
-            update.message.reply_markdown_v2(f'{user.mention_markdown_v2()} ¿En que piensas con esos números\?')
+            context.bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING, timeout=10)
+            sleep(2)
+            update.message.reply_markdown_v2(f'{user.mention_markdown_v2()} esos números son un poco peregrinos\.\.\.')
+
     except ValueError:
         context.bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING, timeout=10)
-        update.message.reply_markdown_v2(f'{user.mention_markdown_v2()} ¿Tan difícil es poner los dados que quieres\?')
+        sleep(2)
+        update.message.reply_markdown_v2(f'{user.mention_markdown_v2()} ¿Tan difícil es poner un número\?')
 
-
-def recomendar_rpg(update: Update, context: CallbackContext):
-    """Recomienda un Juego de Rol al Azar"""
-    rpg = choice(rpg_list)
-    context.bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING, timeout=15)
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=f"Hola, tengo muchos para recomendar, unos {len(rpg_list)}, déjame que piense...")
-    time.sleep(2)
-    context.bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING, timeout=30)
-    time.sleep(5)
-    if rpg == 'FATAL':
-        update.message.reply_text(text=f'Prueba este juego, *_{rpg}_*, seguro que te encanta cuando lo juegues\.',
-                                  parse_mode='MarkdownV2')
-        time.sleep(2)
-        context.bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING, timeout=5)
-        context.bot.send_message(text='Por si no lo pilláis, va en coña...')
-    else:
-        update.message.reply_text(text=f'Prueba este juego, *_{rpg}_*, seguro que te encanta cuando lo juegues\.',
-                                  parse_mode='MarkdownV2')
